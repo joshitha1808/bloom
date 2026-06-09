c26a617688d3d68bff884d33e6fd87e667042cee
joshitha1808 <joshitha0818@gmail.com>
2026-06-09 16:38:13 +0530

    feat: add FirebaseAuth and HTTP client registration to service locator

--- lib/init_dependencies.dart
+++ lib/init_dependencies.dart
@@ -1,6 +1,10 @@
+import 'package:bloom/core/network/connection_check.dart';
 import 'package:bloom/firebase_options.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
+import 'package:http/http.dart' as http;
+import 'package:http/http.dart';
 
 part 'init_dependencies.main.dart';
--- lib/init_dependencies.main.dart
+++ lib/init_dependencies.main.dart
@@ -5,5 +5,9 @@ final GetIt serviceLocator = GetIt.instance;
 Future<void> initDependencies() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  
+  serviceLocator.registerSingleton(FirebaseAuth.instance);
+  serviceLocator.registerSingleton<http.Client>(Client());
+  serviceLocator.registerLazySingleton<ConnectionChecker>(
+    () => ConnectionCheckerImpl(),
+  );
 }
