8c63555b0399bf27edf5a96c353d8daceda6e693
joshitha1808 <joshitha0818@gmail.com>
2026-06-12 22:45:49 +0530

    feat: refactor AudioService to improve asset loading and playback functionality

--- lib/core/services/audio_service.dart
+++ lib/core/services/audio_service.dart
@@ -3,8 +3,11 @@ import 'package:just_audio/just_audio.dart';
 class AudioService {
   final AudioPlayer player = AudioPlayer();
 
-  Future<void> playAsset(String path) async {
+  Future<void> loadAsset(String path) async {
     await player.setAsset(path);
+  }
+
+  Future<void> play() async {
     await player.play();
   }
 
@@ -15,6 +18,18 @@ class AudioService {
   Future<void> stop() async {
     await player.stop();
   }
+
+  Future<void> seek(Duration position) async {
+    await player.seek(position);
+  }
+
+  Stream<Duration> get positionStream => player.positionStream;
+
+  Stream<PlayerState> get playerStateStream => player.playerStateStream;
+
+  Duration? get duration => player.duration;
+
+  bool get isPlaying => player.playing;
 
   Future<void> dispose() async {
     await player.dispose();
