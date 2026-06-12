b396bc506ed3b3a531b19be3734dc35a5d316342
joshitha1808 <joshitha0818@gmail.com>
2026-06-12 22:46:33 +0530

    feat: remove MoodAudio class and its associated audio mapping

--- lib/features/meditation/mood/mood_audio.dart
+++
@@ -1,9 +0,0 @@
-class MoodAudio {
-  static const Map<String, String> audioMap = {
-    "sad": "assets/audio/meditation1.mp3",
-    "stressed": "assets/audio/meditation2.mp3",
-    "angry": "assets/audio/meditation3.mp3",
-    "happy": "assets/audio/meditation4.mp3",
-    "excited": "assets/audio/meditation5.mp3",
-  };
-}
