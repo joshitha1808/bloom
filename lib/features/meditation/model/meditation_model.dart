df827ddcc6687bb82fc9ba519e71bf66eca8daef
joshitha1808 <joshitha0818@gmail.com>
2026-06-12 22:46:14 +0530

    feat: rename MeditationSession to MeditationModel and update properties for clarity

--- lib/features/meditation/model/meditation_model.dart
+++ lib/features/meditation/model/meditation_model.dart
@@ -1,47 +1,51 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'dart:convert';
 
-class MeditationSession {
+class MeditationModel {
   final String id;
   final String userId;
-  final int duration; // seconds
+  final String title;
+  final String audioPath;
+  final int duration;
   final bool completed;
   final DateTime createdAt;
 
-  MeditationSession({
+  MeditationModel({
     required this.id,
     required this.userId,
+    required this.title,
+    required this.audioPath,
     required this.duration,
     required this.completed,
     required this.createdAt,
   });
 
-  // Convert object → Map (for Firebase)
   Map<String, dynamic> toMap() {
     return <String, dynamic>{
       'id': id,
       'userId': userId,
+      'title': title,
+      'audioPath': audioPath,
       'duration': duration,
       'completed': completed,
       'createdAt': createdAt.millisecondsSinceEpoch,
     };
   }
 
-  // Convert Map → object (from Firebase)
-  factory MeditationSession.fromMap(Map<String, dynamic> map) {
-    return MeditationSession(
+  factory MeditationModel.fromMap(Map<String, dynamic> map) {
+    return MeditationModel(
       id: map['id'] as String,
       userId: map['userId'] as String,
+      title: map['title'] as String,
+      audioPath: map['audioPath'] as String,
       duration: map['duration'] as int,
       completed: map['completed'] as bool,
       createdAt: DateTime.fromMillisecondsSinceEpoch(map['createdAt'] as int),
     );
   }
 
-  // Convert object → JSON string
   String toJson() => json.encode(toMap());
 
-  // Convert JSON string → object
-  factory MeditationSession.fromJson(String source) =>
-      MeditationSession.fromMap(json.decode(source) as Map<String, dynamic>);
+  factory MeditationModel.fromJson(String source) =>
+      MeditationModel.fromMap(json.decode(source) as Map<String, dynamic>);
 }
