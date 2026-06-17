f9a5bfdb73e244f6e20b5c19e348cdb57813b79b
joshitha1808 <joshitha0818@gmail.com>
2026-06-17 23:57:55 +0530

    refactor: remove unused copyWith method and override methods from Journal model

--- lib/features/journal/model/journal_model.dart
+++ lib/features/journal/model/journal_model.dart
@@ -1,4 +1,3 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'dart:convert';
 
 class Journal {
@@ -14,22 +13,8 @@ class Journal {
     required this.dateTime,
   });
 
-  Journal copyWith({
-    String? id,
-    String? title,
-    String? content,
-    DateTime? dateTime,
-  }) {
-    return Journal(
-      id: id ?? this.id,
-      title: title ?? this.title,
-      content: content ?? this.content,
-      dateTime: dateTime ?? this.dateTime,
-    );
-  }
-
   Map<String, dynamic> toMap() {
-    return <String, dynamic>{
+    return {
       'id': id,
       'title': title,
       'content': content,
@@ -50,26 +35,4 @@ class Journal {
 
   factory Journal.fromJson(String source) =>
       Journal.fromMap(json.decode(source) as Map<String, dynamic>);
-
-  @override
-  String toString() {
-    return 'Journal(id: $id, title: $title, content: $content, dateTime: $dateTime)';
-  }
-
-  @override
-  bool operator ==(covariant Journal other) {
-    if (identical(this, other)) {
-      return true;
-    }
-
-    return other.id == id &&
-        other.title == title &&
-        other.content == content &&
-        other.dateTime == dateTime;
-  }
-
-  @override
-  int get hashCode {
-    return id.hashCode ^ title.hashCode ^ content.hashCode ^ dateTime.hashCode;
-  }
 }
