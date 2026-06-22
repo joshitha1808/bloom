9a519afbf635f99676db26c09db552c5ced120a0
joshitha1808 <joshitha0818@gmail.com>
2026-06-23 01:09:08 +0530

    feat: add BreathingRepository for managing breathing exercises and fetching data from Firestore

---
+++ lib/features/breathing/repository/breathing_remote_repository.dart
@@ -0,0 +1,27 @@
+import 'package:bloom/features/breathing/model/breathing_model.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:bloom/init_dependencies.dart';
+
+part 'breathing_remote_repository.g.dart';
+
+@riverpod
+BreathingRepository breathingRepository(Ref ref) {
+  final firestore = serviceLocator<FirebaseFirestore>();
+
+  return BreathingRepository(firestore: firestore);
+}
+
+class BreathingRepository {
+  final FirebaseFirestore firestore;
+
+  BreathingRepository({required this.firestore});
+
+  Future<List<BreathingModel>> getExercises() async {
+    final snapshot = await firestore.collection('breathing_exercises').get();
+
+    return snapshot.docs
+        .map((doc) => BreathingModel.fromMap(doc.data()))
+        .toList();
+  }
+}
---
+++ lib/features/breathing/repository/breathing_remote_repository.g.dart
@@ -0,0 +1,58 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'breathing_remote_repository.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint, type=warning
+
+@ProviderFor(breathingRepository)
+final breathingRepositoryProvider = BreathingRepositoryProvider._();
+
+final class BreathingRepositoryProvider
+    extends
+        $FunctionalProvider<
+          BreathingRepository,
+          BreathingRepository,
+          BreathingRepository
+        >
+    with $Provider<BreathingRepository> {
+  BreathingRepositoryProvider._()
+    : super(
+        from: null,
+        argument: null,
+        retry: null,
+        name: r'breathingRepositoryProvider',
+        isAutoDispose: true,
+        dependencies: null,
+        $allTransitiveDependencies: null,
+      );
+
+  @override
+  String debugGetCreateSourceHash() => _$breathingRepositoryHash();
+
+  @$internal
+  @override
+  $ProviderElement<BreathingRepository> $createElement(
+    $ProviderPointer pointer,
+  ) => $ProviderElement(pointer);
+
+  @override
+  BreathingRepository create(Ref ref) {
+    return breathingRepository(ref);
+  }
+
+  /// {@macro riverpod.override_with_value}
+  Override overrideWithValue(BreathingRepository value) {
+    return $ProviderOverride(
+      origin: this,
+      providerOverride: $SyncValueProvider<BreathingRepository>(value),
+    );
+  }
+}
+
+String _$breathingRepositoryHash() =>
+    r'baa7152db1ebc4d6190f8875e93e84414a45554b';
