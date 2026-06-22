f42316cb1d8a5788f5faa342da45cd908b07f2cb
joshitha1808 <joshitha0818@gmail.com>
2026-06-23 00:09:29 +0530

    feat: add breathing feature with initial files and set JournalPage as home

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,21 @@
-import 'package:bloom/features/meditation/view/meditation_list_page.dart';
+import 'package:bloom/features/journal/view/journal_page.dart';
 import 'package:bloom/init_dependencies.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
   await initDependencies();
-
   runApp(const ProviderScope(child: MainApp()));
 }
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: MeditationListPage(),
+      home: JournalPage(uid: '123'),
     );
   }
 }
