d547e73b6dd61f2b5e0037df0929c4c363c3904f
joshitha1808 <joshitha0818@gmail.com>
2026-06-23 16:51:55 +0530

    refactor: remove Firestore dependency and simplify BreathingRepository implementation

--- lib/features/breathing/repository/breathing_local_repository.dart
+++ lib/features/breathing/repository/breathing_local_repository.dart
@@ -1,27 +1,18 @@
+import 'package:bloom/core/constants/breathing_exercises.dart';
 import 'package:bloom/features/breathing/model/breathing_model.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
-import 'package:bloom/init_dependencies.dart';
 
 part 'breathing_local_repository.g.dart';
 
 @riverpod
 BreathingRepository breathingRepository(Ref ref) {
-  final firestore = serviceLocator<FirebaseFirestore>();
-
-  return BreathingRepository(firestore: firestore);
+  return BreathingRepository();
 }
 
 class BreathingRepository {
-  final FirebaseFirestore firestore;
-
-  BreathingRepository({required this.firestore});
-
   Future<List<BreathingModel>> getExercises() async {
-    final snapshot = await firestore.collection('breathing_exercises').get();
+    await Future.delayed(const Duration(milliseconds: 300));
 
-    return snapshot.docs
-        .map((doc) => BreathingModel.fromMap(doc.data()))
-        .toList();
+    return breathingExercises;
   }
 }
