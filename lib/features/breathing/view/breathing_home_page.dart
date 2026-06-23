01ad980bc3a532616c4fc62989697017ec6e8e5a
joshitha1808 <joshitha0818@gmail.com>
2026-06-23 16:52:01 +0530

    feat: implement BreathingHomePage with exercise fetching and navigation

---
+++ lib/features/breathing/view/breathing_home_page.dart
@@ -0,0 +1,62 @@
+import 'package:bloom/features/breathing/view/breathing_session_page.dart';
+import 'package:bloom/features/breathing/viewmodel/breathing_viewmodel.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class BreathingHomePage extends ConsumerStatefulWidget {
+  const BreathingHomePage({super.key});
+
+  @override
+  ConsumerState<BreathingHomePage> createState() => _BreathingHomePageState();
+}
+
+class _BreathingHomePageState extends ConsumerState<BreathingHomePage> {
+  @override
+  void initState() {
+    super.initState();
+
+    Future.microtask(() {
+      ref.read(breathingViewModelProvider.notifier).fetchExercises();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final exercises = ref.watch(breathingViewModelProvider);
+
+    return Scaffold(
+      appBar: AppBar(title: const Text('Breathing')),
+      body: exercises.when(
+        data: (list) {
+          return ListView.builder(
+            padding: const EdgeInsets.all(16),
+            itemCount: list.length,
+            itemBuilder: (context, index) {
+              final exercise = list[index];
+
+              return Card(
+                margin: const EdgeInsets.only(bottom: 16),
+                child: ListTile(
+                  title: Text(exercise.name),
+                  subtitle: Text(exercise.description),
+                  trailing: const Icon(Icons.arrow_forward_ios),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (_) =>
+                            BreathingSessionPage(exercise: exercise),
+                      ),
+                    );
+                  },
+                ),
+              );
+            },
+          );
+        },
+        loading: () => const Center(child: CircularProgressIndicator()),
+        error: (e, _) => Center(child: Text(e.toString())),
+      ),
+    );
+  }
+}
