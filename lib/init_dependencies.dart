03311fa35a26547b2794f0acdc4236efe7ee2bb8
joshitha1808 <joshitha0818@gmail.com>
2026-06-25 00:33:21 +0530

    feat: add FirebaseFirestore dependency injection

--- lib/init_dependencies.dart
+++ lib/init_dependencies.dart
@@ -1,5 +1,6 @@
 import 'package:bloom/core/network/connection_check.dart';
 import 'package:bloom/firebase_options.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
