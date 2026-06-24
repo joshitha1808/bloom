2232708487d1fba025b56f4620769949c88e0fad
joshitha1808 <joshitha0818@gmail.com>
2026-06-25 00:25:25 +0530

    feat: add FirebaseFirestore singleton

--- lib/init_dependencies.main.dart
+++ lib/init_dependencies.main.dart
@@ -4,9 +4,22 @@ final GetIt serviceLocator = GetIt.instance;
 
 Future<void> initDependencies() async {
   WidgetsFlutterBinding.ensureInitialized();
+
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  serviceLocator.registerSingleton(FirebaseAuth.instance);
-  serviceLocator.registerSingleton<http.Client>(Client());
+
+  // Firebase
+  serviceLocator.registerLazySingleton<FirebaseAuth>(
+    () => FirebaseAuth.instance,
+  );
+
+  serviceLocator.registerLazySingleton<FirebaseFirestore>(
+    () => FirebaseFirestore.instance,
+  );
+
+  // HTTP
+  serviceLocator.registerLazySingleton<http.Client>(() => Client());
+
+  // Connection Checker
   serviceLocator.registerLazySingleton<ConnectionChecker>(
     () => ConnectionCheckerImpl(),
   );
